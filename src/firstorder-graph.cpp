c1a36f9a6dfcfea105f27139aea7aee1e3be4ebb
Kai Daniel <kaiingodaniel@gmail.com>
2023-02-28 23:58:44 +0000

    start walking graph

--- src/firstorder-graph.cpp
+++ src/firstorder-graph.cpp
@@ -10,35 +10,42 @@
 #include <tree_sitter/parser.h>
 #include <vector>
 
+using std::string;
+using std::to_string;
+using std::unique_ptr;
+using str = const std::string &;
+using std::begin;
+using std::cout;
+using std::end;
+using std::transform_reduce;
+using std::visit;
+
 extern "C" {
 TSLanguage *tree_sitter_python(void);
 }
 namespace stanly {
 
 template <class... Args> void FirstOrderGraph::insert(Args &&...args) {
-  nodes_.emplace_back(args...);
+  nodes_.emplace_back(std::forward<Args>(args)...);
 };
 
-std::string show(const DeclareLocalVar &) { return "(DeclareLocalVar ?)"; }
-std::string show(const SetField &) { return "(SetField ? ? ?)"; }
-std::string show(const LoadField &n) {
-  return std::string("(LoadField ") + std::to_string(n.lhs) + " " +
-      std::to_string(n.source) + " " + std::to_string(n.field) + ")";
+string show(const DeclareLocalVar &) { return "(DeclareLocalVar ?)"; }
+string show(const SetField &) { return "(SetField ? ? ?)"; }
+string show(const LoadField &n) {
+  return string("(LoadField ") + to_string(n.lhs) + " " + to_string(n.source) +
+      " " + to_string(n.field) + ")";
 }
-std::string show(const LoadText &n) {
-  return std::string("(LoadText ") + n.text_literal + " " +
-      std::to_string(n.lhs) + ")";
+string show(const LoadText &n) {
+  return string("(LoadText ") + n.text_literal + " " + to_string(n.lhs) + ")";
 }
-std::string show(const LoadRecord &) { return "(LoadRecord ? ?)"; }
-std::string show(const LoadVar &) { return "(LoadVar ? ?)"; }
-std::string show(const FirstOrderGraph &graph) {
-  return std::transform_reduce(
-      std::begin(graph.nodes_), std::end(graph.nodes_), std::string{},
-      [](const std::string &str1, const std::string &str2) {
-        return str1 + str2;
-      },
+string show(const LoadRecord &) { return "(LoadRecord ? ?)"; }
+string show(const LoadVar &) { return "(LoadVar ? ?)"; }
+string show(const FirstOrderGraph &graph) {
+  return transform_reduce(
+      begin(graph.nodes_), end(graph.nodes_), string{},
+      [](str str1, str str2) { return str1 + str2; },
       [](const auto &syntax) {
-        return std::visit([](const auto &node) { return show(node); }, syntax);
+        return visit([](const auto &node) { return show(node); }, syntax);
       });
 }
 
@@ -49,8 +56,13 @@ namespace treesitter { // every use of tree-sitter in this namespace
     TSParser *parser_;
     TSTree *tree_;
     TSNode root_;
+    TSTreeCursor cursor_;
+    struct {
+      TSFieldId statement;
+      } fields_;
+    FirstOrderGraph graph_{};
   public:
-    explicit Parser(const std::string &program)
+    explicit Parser(str program)
         : language_(tree_sitter_python()),
           parser_(ts_parser_new()),
           tree_([&] {
@@ -58,7 +70,13 @@ namespace treesitter { // every use of tree-sitter in this namespace
             return ts_parser_parse_string(
                 parser_, nullptr, program.c_str(), program.length());
           }()),
-          root_(ts_tree_root_node(tree_)) {}
+          root_(ts_tree_root_node(tree_)),
+          cursor_(ts_tree_cursor_new(root_)),
+          fields_({
+            .statement=field_id("expression_statement")
+            
+          })
+          {}
     ~Parser() {
       ts_tree_delete(tree_);
       ts_parser_delete(parser_);
@@ -69,37 +87,33 @@ namespace treesitter { // every use of tree-sitter in this namespace
     Parser operator=(Parser &&) = delete;
 
     [[nodiscard]] const TSNode &root() const { return root_; }
-    [[nodiscard]] static std::unique_ptr<char> s_expr(const TSNode& node) {
-       return std::unique_ptr<char>{ts_node_string(node)};}
-    [[nodiscard]] TSFieldId field_id(const std::string& name) const { 
-      return ts_language_field_id_for_name(language_, name.c_str(), name.length());}
+    [[nodiscard]] static unique_ptr<char> s_expr(const TSNode &node) {
+      return unique_ptr<char>{ts_node_string(node)};
+    }
+    [[nodiscard]] TSFieldId field_id(str name) const {
+      return ts_language_field_id_for_name(
+          language_, name.c_str(), name.length());
+    }
+    template <class... Args> void insert(Args&& ...args) { graph_.insert(std::forward<Args>(args)...); }
+    bool child() { return ts_tree_cursor_goto_first_child(&cursor_); }
+    bool sibling() { return ts_tree_cursor_goto_next_sibling(&cursor_); }
+    bool statement() { return ts_tree_cursor_current_field_id(&cursor_) == fields_.statement; }
+
+    [[nodiscard]] FirstOrderGraph operator()() && {
+      cout << s_expr(root());
+      if(child()){
+        while(sibling()) {
+          if(statement()){
+            insert(LoadText{0, "x"});
+          }
+        }
+      }
+      return graph_;
+    }
   };
-  /* interface for Parser:
-      root:   ()  -> Node
-      child: Node -> Node
-      name:  Node -> String */
-  static FirstOrderGraph parse_firstorder(const std::string &program) {
-    const Parser parser{program};
-    TSNode child = ts_node_named_child(parser.root(), 0);
-    std::string name = ts_node_type(child);
-    FirstOrderGraph graph{};
-    graph.insert(LoadText{0, name});
-    graph.insert(LoadField{0, 1, 2});
-    // std::cout << Parser::s_expr(child);
-    // const TSFieldId STATEMENT = parser.field_id("expression_statement");
-    std::cout << Parser::s_expr(parser.root());
-    // TSTreeCursor cursor{ts_tree_cursor_new(parser.root())};
-    // if(ts_tree_cursor_goto_first_child(&cursor)){
-    //   while(ts_tree_cursor_goto_next_sibling(&cursor)){
-    //     TSFieldId field = ts_tree_cursor_current_field_id(&cursor);
-    //     if(field == STATEMENT) {
 
-    //     }
-    //   }
-    // }
-
-
-    return graph;
+  static FirstOrderGraph parse_firstorder(str program) {
+    return Parser{program}();
   }
 } // namespace treesitter
 
