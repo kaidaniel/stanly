0dfebb01379853263b085a7bb398113ebb742774
Kai Daniel <kaiingodaniel@gmail.com>
2023-03-05 20:28:25 +0000

    add alias template

--- src/firstorder-format.cpp
+++ src/firstorder-format.cpp
@@ -8,11 +8,16 @@
 
 #include <type_traits>
 
+using stanly::metaprogramming::is_any_of_v;
+using stanly::metaprogramming::type_name;
+using stanly::metaprogramming::struct_to_tuple;
+template<class T>
+using is_firstorder_syntax_node = std::enable_if_t<is_any_of_v<T, stanly::FirstOderSyntaxNodes>, char>;
 
 template<typename T>
-struct fmt::formatter<T, std::enable_if_t<stanly::metaprogramming::is_any_of_v<T, stanly::FirstOderSyntaxNodes>, char>> : fmt::formatter<string_view> {
+struct fmt::formatter<T, is_firstorder_syntax_node<T>> : fmt::formatter<string_view> {
   template <class FormatContext>
   auto format(const T& n, FormatContext& ctx) const {
-    return formatter<string_view>::format("({} {})", stanly::metaprogramming::type_name<T>, stanly::metaprogramming::struct_to_tuple<T>(n),  ctx);
+    return formatter<string_view>::format("({} {})", type_name<T>, struct_to_tuple<T>(n),  ctx);
   }
 };
