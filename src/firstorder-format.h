fb172e6a75f99730707605b3880ae941b0fc0452
Kai Daniel <kaiingodaniel@gmail.com>
2023-04-11 22:27:53 +0100

    make tests not depend on string repr

--- src/firstorder-format.h
+++ src/firstorder-format.h
@@ -42,7 +42,10 @@ const std::string type_name_suffix = [] {
 
 auto to_tpl(auto &&object) noexcept {
   using type = std::decay_t<decltype(object)>;
-  if constexpr (requires(type t) { type{{}, {}, {}, {}}; }) {
+  if constexpr (requires(type t) { type{{}, {}, {}, {}, {}}; }) {
+    auto &&[p1, p2, p3, p4, p5] = object;
+    return std::make_tuple(p1, p2, p3, p4, p5);
+  } else if constexpr (requires(type t) { type{{}, {}, {}, {}}; }) {
     auto &&[p1, p2, p3, p4] = object;
     return std::make_tuple(p1, p2, p3, p4);
   } else if constexpr (requires(type t) { type{{}, {}, {}}; }) {
--- src/test-firstorder-parser.cpp
+++ src/test-firstorder-parser.cpp
@@ -1,4 +1,3 @@
-
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators_all.hpp>
 #include <vector>
@@ -9,28 +8,68 @@
 
 using std::string;
 namespace stanly::firstorder {
+using stx = syntax<std::string_view>;
 constexpr auto parse = parse_language<syntax<std::string_view>>;
-
-TEST_CASE("single statements", "[first-order][parsing]") {
-  auto v = GENERATE(chunk(2, values({
-                                 // clang-format off
-    "x=y",                  "[inj-load_var(x y)]",
-    "x=1",                  "[inj-load_text(x 1)]",
-    "y=[]",                 "[inj-load_top(y [])]",
-    "z = {}",               "[inj-load_record(z [])]",
-    "z = {1: 'x', 3: {}}",  "[inj-load_record(z [1, 3])]",
-    "abc = {1: 'x'}",       "[inj-load_record(abc [1])]",
-    "abc_def = {1,2,3}",    "[inj-load_top(abc_def {1,2,3})]",
-    "a[b] = x",             "[inj-set_field(x a b)]",
-    "x = a[b]",             "[inj-load_field(x a b)]",
-    "x = y",                "[inj-load_var(x y)]",
-    "x = 1",                "[inj-load_text(x 1)]",
-                                 // clang-format on
-                             })));
-  auto statement = parse(v[0]);
-  REQUIRE(std::format("{}", statement) == v[1]);
+template <class Node>
+bool parses_to(std::string_view program, const Node& node) {
+  auto parsed_variant{parse(program).at(0)};
+  try {
+    Node parsed_node{std::get<Node>(parsed_variant)};
+    return to_tpl(parsed_node) == to_tpl(node);
+  } catch (std::bad_variant_access&) {
+    constexpr std::string_view msg{"expected \"{}\" to parse to 'inj-{}', but got '{}'."};
+    UNSCOPED_INFO(std::format(msg, program, node, parsed_variant));
+    return false;
+  }
 }
-TEST_CASE("multiple statements", "[first-order][parsing]") {
+TEST_CASE("single statements", "[firstorder][parsing]") {
+  CHECK(parses_to("x=y", stx::load_var{"x", "y"}));
+  CHECK(parses_to("x=1", stx::load_text{"x", "1"}));
+  CHECK(parses_to("y=[]", stx::load_top{"y", "[]"}));
+  CHECK(parses_to("z = {}", stx::load_record{"z", {}}));
+  CHECK(parses_to("z = {1: 'x', 3: {}}", stx::load_record{"z", {"1", "3"}}));
+  CHECK(parses_to("abc = {1: 'x'}", stx::load_record{"abc", {"1"}}));
+  CHECK(parses_to("abc_def = {1,2,3}", stx::load_top{"abc_def", "{1,2,3}"}));
+  CHECK(parses_to("a[b] = x", stx::set_field{"x", "a", "b"}));
+  CHECK(parses_to("x = a[b]", stx::load_field{"x", "a", "b"}));
+  CHECK(parses_to("x = y", stx::load_var{"x", "y"}));
+  CHECK(parses_to("x = 1", stx::load_text{"x", "1"}));
+}
+
+template <class Tpl, std::size_t... Idxs>
+bool parses_to_multiple_(std::string_view program, const Tpl& nodes, std::index_sequence<Idxs...>) {
+  std::vector parsed_vector{parse(program)};
+  if (sizeof...(Idxs) != parsed_vector.size()) {
+    constexpr std::string_view msg{"expected {} nodes, but parsed {}: {}"};
+    UNSCOPED_INFO(std::format(msg, sizeof...(Idxs), parsed_vector.size(), parsed_vector));
+    return false;
+  }
+  auto is_type = [&]<class N>(const N& node, const std::size_t i) -> bool {
+    try {
+      N parsed_node{std::get<N>(parsed_vector.at(i))};
+      bool result{to_tpl(parsed_node) == to_tpl(node)};
+      if (!result) { UNSCOPED_INFO(std::format("expected {}, but got {}", node, parsed_node)); }
+      return result;
+    } catch (std::bad_variant_access&) {
+      constexpr std::string_view msg{"statement {} of \"{}\" parsed to wrong node '{}'"};
+      UNSCOPED_INFO(std::format(msg, i + 1, program, parsed_vector.at(i)));
+      return false;
+    }
+  };
+  return (is_type(std::get<Idxs>(nodes), Idxs) && ...);
+}
+
+template<class... Arg>
+bool parses_to(std::string_view program, const std::tuple<Arg...>& nodes) {
+  auto idxs = std::make_index_sequence<std::tuple_size_v<std::decay_t<decltype(nodes)>>>{};
+  return parses_to_multiple_(program, nodes, idxs);
+}
+
+TEST_CASE("multiple statements2", "[firstorder][parsing]") {
+  CHECK(parses_to("z={}; x=a[b]",
+                           std::tuple{stx::load_record{"z", {}}, stx::load_field{"x", "a", "b"}}));
+}
+TEST_CASE("multiple statements", "[.firstorder][parsing]") {
   auto v =
       GENERATE(chunk(2, values({
                             // clang-format off
