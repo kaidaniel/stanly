cdb0e06f719f6b35805920e34e79891eea79174e
Kai Daniel <kaiingodaniel@gmail.com>
2023-05-19 18:58:29 +0100

    choose domains

--- src/firstorder-analyse.h
+++ src/firstorder-analyse.h
@@ -1,82 +1,58 @@
-#include <string_view>
-#include <variant>
-#include <vector>
+#include <AbstractDomain.h>
+#include <ConstantAbstractDomain.h>
+#include <DisjointUnionAbstractDomain.h>
+#include <HashedAbstractPartition.h>
+
+#include <concepts>
 
 #include "ConstantAbstractDomain.h"
 #include "DirectProductAbstractDomain.h"
 #include "HashedAbstractEnvironment.h"
 #include "HashedSetAbstractDomain.h"
-#include "firstorder-syntax.h"
 
-namespace stanly {
+namespace stanly::firstorder {
 
-template <typename D>
-concept abstract_domain =
-    std::derived_from<typename D::domain, sparta::AbstractDomain<typename D::domain>> &&
-    requires { typename D::domain; };
+// reduced prod: intersection of denot. single bottom implies prod bottom. reduction is called e.g.
+// in if-statements. maybe also fn args? direct prod: prod. bottom implies all bottom. Domain:
+// memory x scope memory: address -> object  partition (implicit bottom; single top does _not_ imply
+// partition top) scope: var -> [address]    env (implicit top; single bottom implies scope bottom)
+// var, address, field: int          const
+// [x]: set[x]                powerset of integers
+// object: type x value       direct-prod (or: reduced prod for type-value mismatch)
+// value: record + constant   disjoint-sum
+// record: def bindings x use [fields]   direct-prod
+// bindings: field -> [address]    partition
+// type: dict, dataframe, closure, int, top, ...
 
-namespace firstorder {
-
-using Kind = sparta::AbstractValueKind;
-/// Abstraction of the program state (Var -> Value).
-template <class Domain>
-struct domain_mixin : sparta::AbstractDomain<Domain> {
-  using domain = Domain;
-  template <class... Args>
-  domain_mixin(Args&&... args) : domain_(std::forward<Args>(args)...) {}
-
- private:
-  domain domain_{};
-};
-template <class Repr>
-struct domain {
-  using str_mixin = domain_mixin<sparta::ConstantAbstractDomain<Repr>>;
-  using record_mixin = domain_mixin<sparta::HashedSetAbstractDomain<Repr>>;
-  template <class S, class O>
-  using env_mixin = domain_mixin<sparta::HashedAbstractEnvironment<S, O>>;
-  template <class O, class S, class R>
-  using object_mixin = sparta::DirectProductAbstractDomain<O, S, R>;
-  struct str : str_mixin {
-    using str_mixin::domain_mixin;
-  };
-  struct record : record_mixin {
-    using record_mixin::domain_mixin;
-  };
-  struct object : object_mixin<object, str, record> {
-    using object_mixin<object, str, record>::object_mixin;
-  };
-
-  struct env : env_mixin<str, object> {
-    using env_mixin<str, object>::env_mixin;
-  };
-  using types = std::tuple<str, record, object, env>;
-  struct top {};
-  struct bottom {};
-  using recordv = record;
-};
-auto const analyse = [](std::vector<firstorder::syntax<idx>::node>) -> domain<std::size_t>::env {
-  return {};
-};
-
-// static_assert(abstract_domain<domain<idx>>);
-}  // namespace firstorder
-}  // namespace stanly
-// TODO: replace with actual abstract domain in src/firstorder-syntax.h
-// template<class Repr>
-// struct domain {
-//   struct str { Repr val; };
-//   struct record { Repr field1; Repr field2; };
-//   struct recordv { Repr field1; Repr field2; };
-//   struct top{};
-//   struct bottom{};
-//   using object = std::variant<str, record, recordv, top, bottom>;
-//   using env = std::unordered_map<Repr, object>;
-//  };
-
-namespace stanly {
 template <class T>
-  requires contains<firstorder::domain<std::size_t>::object, std::decay_t<T>>
-struct is_syntax_node<T> {
-  constexpr static bool value = true;
+concept abstract_domain = std::derived_from<T, sparta::AbstractDomain<T>>;
+
+template <class Field = short, class Address = short, class Var = short, class Constant = short,
+          class Type = short>
+struct abstract_domain_types {
+  struct concrete {
+    using field = Field;
+    using address = Address;
+    using var = Var;
+    using constant = Constant;
+    using type = Type;
+  };
+  using addresses = sparta::HashedSetAbstractDomain<Address>;
+  using constant = sparta::ConstantAbstractDomain<Constant>;
+  using bindings = sparta::DisjointUnionAbstractDomain<Field, addresses>;
+  using fields = sparta::HashedSetAbstractDomain<Field>;
+  struct record : sparta::DirectProductAbstractDomain<record, bindings, fields> {};
+  using value = sparta::DisjointUnionAbstractDomain<record, constant>;
+  using type = sparta::ConstantAbstractDomain<Type>;
+  struct object : sparta::DirectProductAbstractDomain<object, type, value> {};
+
+  using scope = sparta::HashedAbstractEnvironment<Var, addresses>;
+  using memory = sparta::HashedAbstractPartition<Address, object>;
+  struct state : sparta::DirectProductAbstractDomain<state, scope, memory> {};
+  static_assert(abstract_domain<state>);
+  static_assert(abstract_domain<scope>);
+  static_assert(abstract_domain<memory>);
 };
-}  // namespace stanly
+
+// using abstract_domain = abstract_domain_types<>::state;
+}  // namespace stanly::firstorder
--- src/test-analyse.cpp
+++ src/test-analyse.cpp
@@ -3,30 +3,32 @@
 #include <string_view>
 #include <vector>
 
-#include "catch2/matchers/catch_matchers_range_equals.hpp"
+// #include "catch2/matchers/catch_matchers_range_equals.hpp"
 #include "firstorder-analyse.h"
-#include "firstorder-syntax.h"
-#include "syntax.h"
+// #include "firstorder-syntax.h"
+// #include "syntax.h"
 
-namespace stanly::firstorder {
-struct programs : syntax<idx> {
-  std::vector<std::vector<node>> operator()() {
-    return {{text{idx{0}, idx{1}}, alloc{idx{2}, idx{100}}, update{idx{2}, idx{3}, idx{0}},
-             update{idx{2}, idx{4}, idx{0}}},
-            {alloc{idx{0}, idx{101}}, load{idx{3}, idx{0}, idx{1}}, load{idx{4}, idx{0}, idx{5}},
-             ref{idx{6}, idx{0}}},
-            {alloc{idx{0}, idx{9}}, update{idx{0}, idx{2}, idx{0}}, load{idx{3}, idx{0}, idx{4}}}};
-  }
-};
-struct bindings : domain<std::size_t> {
-  std::vector<env> operator()() {
-    return {{{0, str{1}}, {2, record{3, 4}}},
-            {{0, record{1, 2}}, {3, top{}}, {4, bottom{}}, {6, bottom{}}},
-            {{0, recordv{2, 4}}, {3, top{}}}};
-  }
-};
+// namespace stanly::firstorder {
+// struct programs : syntax<idx> {
+//   std::vector<std::vector<node>> operator()() {
+//     return {{text{idx{0}, idx{1}}, alloc{idx{2}, idx{100}}, update{idx{2}, idx{3}, idx{0}},
+//              update{idx{2}, idx{4}, idx{0}}},
+//             {alloc{idx{0}, idx{101}}, load{idx{3}, idx{0}, idx{1}}, load{idx{4}, idx{0}, idx{5}},
+//              ref{idx{6}, idx{0}}},
+//             {alloc{idx{0}, idx{9}}, update{idx{0}, idx{2}, idx{0}}, load{idx{3}, idx{0},
+//             idx{4}}}};
+//   }
+// };
+// struct bindings : domain<std::size_t> {
+//   std::vector<env> operator()() {
+//     return {{{0, constant{1}}, {2, record::def(3, 4)}},
+//             {{0, record::def(1, 2)}, {3, top{}}, {4, bottom{}}, {6, bottom{}}},
+//             {{0, record::use(2, 4)}, {3, top{}}}};
+//   }
+// };
 
-TEST_CASE("analyse firstorder programs", "[firstorder][.analyse]") {
-  CHECK_THAT(bindings{}(), Catch::Matchers::RangeEquals(vw::transform(programs{}(), analyse)));
+TEST_CASE("analyse firstorder programs", "[firstorder][analyse]") {
+  CHECK(1 + 5 == 1 + 5);
+  // CHECK_THAT(bindings{}(), Catch::Matchers::RangeEquals(vw::transform(programs{}(), analyse)));
 }
-}  // namespace stanly::firstorder
+// }  // namespace stanly::firstorder
