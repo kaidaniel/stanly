6d0f4a48c1db09d3ff0904b9e31faeb28a01d0f9
Kai Daniel <kaiingodaniel@gmail.com>
2023-12-22 16:51:48 +0000

    more sane default

--- ghc/app/Main.hs
+++ ghc/app/Main.hs
@@ -14,19 +14,19 @@ import Stanly.Store (store, value)
 import Stanly.Unicode
 import Stanly.Val (prune)
 
-data Options = Options {valueO, storeO, pruneO, desugaredO, astO, traceO, deadCodeO, noColourO, sectionHeadersO ∷ Bool} deriving (Read, Show, Eq)
+data Options = Options {noValueO, storeO, pruneO, desugaredO, astO, traceO, deadCodeO, noColourO, sectionHeadersO ∷ Bool} deriving (Read, Show, Eq)
 
 options ∷ Parser Options
 options =
     Options
-        <$> flag "value" 'v' "Show calculated value (default if no other options set)."
+        <$> flag "no-value" 'n' "Don't show calculated value."
             ⊛ flag "store" 's' "Show the final state of the store after the program halts."
             ⊛ flag "prune" 'p' "Prune store before showing (when using --store)."
             ⊛ flag "desugared" 'd' "Show the program after syntax transformation."
             ⊛ flag "ast" 'a' "Show the abstract syntax tree used by the interpreter."
             ⊛ flag "trace" 't' "Show how the interpreter state changes while the program is being evaluated."
             ⊛ flag "dead-code" 'e' "Show parts of the program that weren't reached during interpretation."
-            ⊛ flag "no-colour" 'n' "Don't colourise output."
+            ⊛ flag "no-colour" 'c' "Don't colourise output."
             ⊛ flag "section-headers" 'i' "Introduce each section by a line of '===' and the section's name."
   where
     flag l s h = switch (long l ⋄ short s ⋄ help h)
@@ -41,11 +41,10 @@ outputs ∷ Options → Expr → [FmtStr]
 outputs Options{..} ast =
     let
         concrete₁ = runIdentity ⎴ concrete idₘ ast
-        isDefault = not (storeO || pruneO || desugaredO || astO || traceO || deadCodeO || noColourO || sectionHeadersO)
         m ++? (b, title, m₁) = if b then m ++ [if sectionHeadersO then "== " ⊹ title ⊹\ m₁ else m₁] else m
         sections =
             ε₁
-                ++? (valueO || isDefault, "value", fmt ⎴ value concrete₁)
+                ++? (not noValueO, "value", fmt ⎴ value concrete₁)
                 ++? (storeO, "store", fmt ⎴ (if pruneO then φ prune else id) (store concrete₁))
                 ++? (desugaredO, "desugared", fmt ast)
                 ++? (astO, "ast", fmt (show ast))
