ee1b82ba807610559f8a0596214c979d1c0da229
Kai Daniel <kaiingodaniel@gmail.com>
2024-01-01 23:17:34 +0000

    fail more loudly

--- ghc/tests/test.sh
+++ ghc/tests/test.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
+nok=0
+nfailure=0
 red() { printf "\e[31m%s\e[0m\n" "$1"; }
 green() { printf "\e[32m%s\e[0m\n" "$1"; }
-
-error() { 
-    red "Error: $1"
-    exit 1
-}
+failure() { red "✘ $1"; nfailure=$((nfailure+1)); }
+ok() { green "✔ $1"; nok=$((nok+1)); }
 
 export PATH=$PATH:build/
+files="tests/*.adi"
 
-for file in tests/*.adi
+for file in $files
 do 
     # shellcheck disable=SC2059
     cmd="$(printf "$(sed -n -E 's|^// RUN:\s*(.*)|\1|p' "$file")" "$file" "$file")"
-    echo "$cmd"
-    bash -c "$cmd"
+    if bash -c "$cmd"; then f=ok; else f=failure; fi
+    $f "$cmd"
 done
 
-error "Need to fix test.sh to show red error message when FileCheck has nonzero return status."
+if [[ nfailure -eq 0 ]]; then g=green; else g=red; fi
+$g "$files: $nok ok, $nfailure failures"
